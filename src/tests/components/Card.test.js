a0524a6bd52724c088374bfe66f401b4cfb9cb73
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-03-27 21:07:01 +0100

    added snapshot testing

---
+++ src/tests/components/Card.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Card from "../../components/Card";
+
+describe("Card", () => {
+  const validProps = {
+    date: 1111111,
+    description: "Stub description",
+    icon: "stubIcon",
+    temperature: {
+      min: 12,
+      max: 22,
+    },
+  };
+
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <Card
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
