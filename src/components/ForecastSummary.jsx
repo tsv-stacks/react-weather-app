8891b296c8b2563b978b0e3e554d8ffb9c9a7732
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-04-02 22:55:23 +0100

    added error handling£

--- src/components/ForecastSummary.jsx
+++ src/components/ForecastSummary.jsx
@@ -6,8 +6,11 @@ import { getDay } from "./DataFunction";
 
 export default function Card({ userInput }) {
   const [forecastData, setForecastData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
+    let timeoutId;
+
     axios
       .get(
         `https://cmd-shift-weather-app.onrender.com/forecast?city=${
@@ -17,22 +20,48 @@ export default function Card({ userInput }) {
       .then((response) => {
         setForecastData(response.data);
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((error) => {
+        if (error.response) {
+          if (error.response.status === 500) {
+            setErrorMessage("Oops, server error, try again later.");
+            console.error("Server error", error);
+          } else if (error.response.status === 404) {
+            setErrorMessage("Oops, city not found.");
+            console.error("City not found", error);
+          } else {
+            setErrorMessage("Oops, something went wrong.");
+            console.error("Error", error);
+          }
+
+          timeoutId = setTimeout(() => {
+            setErrorMessage(null);
+          }, 5000);
+        }
       });
+    return () => clearTimeout(timeoutId);
   }, [userInput]);
 
   return (
-    <section className="cards">
-      {forecastData &&
-        forecastData.forecasts.map((card, i) => {
-          return (
-            <Forecast
-              key={getDay(forecastData.forecasts[i].date)}
-              data={forecastData.forecasts[i]}
-            />
-          );
-        })}
-    </section>
+    <>
+      {errorMessage && (
+        <div className="error-message">
+          <h2>{errorMessage}</h2>
+        </div>
+      )}
+      <section className="cards">
+        {forecastData ? (
+          forecastData.forecasts.map((card, i) => {
+            return (
+              <Forecast
+                key={getDay(forecastData.forecasts[i].date)}
+                data={forecastData.forecasts[i]}
+              />
+            );
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
+      </section>
+    </>
   );
 }
