4a0aa8c306609eaf5ee7bf13c0f5f794731b3853
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-04-06 21:55:27 +0100

    removed error handling for api

--- src/components/App.js
+++ src/components/App.js
@@ -11,14 +11,12 @@ import "../styles/SearchForm.css";
 function App() {
   const [location, setLocation] = useState(null);
   const [userInput, setUserInput] = useState("London");
-  const [errorMessage, setErrorMessage] = useState(null);
 
   const handleSearch = (input) => {
     setUserInput(input);
   };
 
   useEffect(() => {
-    let timeoutId;
     const API_KEY = process.env.REACT_APP_API_KEY;
     const URL = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&APPID=${API_KEY}`;
     axios
@@ -26,36 +24,26 @@ function App() {
       .then((response) => {
         setLocation(response.data);
       })
-      .catch((error) => {
-        if (error.response.status) {
-          setErrorMessage(error.response.data.message);
-        }
-        timeoutId = setTimeout(() => {
-          setErrorMessage(null);
-        }, 5000);
-      });
-    return () => clearTimeout(timeoutId);
-  }, [userInput]);
+      .catch(
+        (error) => {
+          return error;
+        },
+        [userInput]
+      );
+  });
 
   return (
-    <>
-      {errorMessage && (
-        <div className="error-message">
-          <h2>{errorMessage}</h2>
-        </div>
+    <div className="App">
+      {location ? (
+        <>
+          <Location location={location} />
+          <SearchForm onSearch={handleSearch} />
+          <ForecastSummary userInput={userInput} />
+        </>
+      ) : (
+        <p>Loading...</p>
       )}
-      <div className="App">
-        {location ? (
-          <>
-            <Location location={location} />
-            <SearchForm onSearch={handleSearch} />
-            <ForecastSummary userInput={userInput} />
-          </>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-    </>
+    </div>
   );
 }
 
